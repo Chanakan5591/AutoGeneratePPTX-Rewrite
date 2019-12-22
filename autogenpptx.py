abdc1878c14ecab9d016200de9ea6c2f27fbaee2
Chanakan Mungtin <chanakan5591@gmail.com>
2019-12-22 15:44:11 +0700

    Edit the code to match the PEP8 Requirement.

--- autogenpptx.py
+++ autogenpptx.py
@@ -5,14 +5,31 @@ import argparse
 import sys
 import signal
 
-parser = argparse.ArgumentParser(description="This tool will help you create your presentation slide fast and easy. Created by Chanakan Mungtin")
-parser.add_argument("-i","--input",help="Specify powerpoint draft file created by format within 'format.txt'", nargs='?', default=None, required=True)
-parser.add_argument("-p","--pages",help="Specify how many page will be in the presentation -- DEFAULT 1", nargs='?', default=0, required=True, type=int)
-parser.add_argument("-o","--output",help="Specify ouput filename for the ouput file. -- DEFAULT output.pptx", nargs='?', default="output.pptx", required=False, type=str)
+parser = argparse.ArgumentParser(
+                                 description="This tool will help you create" +
+                                 " your presentation slide fast and easy. " +
+                                 "Created by Chanakan Mungtin"
+                                )
+parser.add_argument(
+                    "-i", "--input", help="Specify powerpoint draft file " +
+                    "created by format within 'format.txt'", nargs='?',
+                    default=None, required=True
+                   )
+parser.add_argument(
+                    "-p", "--pages", help="Specify how many page will be in " +
+                    " the presentation -- DEFAULT 1", nargs='?', default=0,
+                    required=True, type=int
+                   )
+parser.add_argument(
+                    "-o", "--output", help="Specify ouput filename for the " +
+                    "ouput file. -- DEFAULT output.pptx", nargs='?',
+                    default="output.pptx", required=False, type=str
+                   )
 
 args = parser.parse_args()
 
 outfilename = str(args.output)
+
 
 def inputFileRead(filename):
     try:
@@ -23,16 +40,19 @@ def inputFileRead(filename):
         print("Error! File inaccessible")
         sys.exit(1)
 
+
 def savePPTX(outputPath):
     if "/" in outputPath:
-        outputFilename = list(str(outputPath).split("/")) #Note to self. Test if work. Unsure.
+        # Note to self. Test if work. Unsure.
+        outputFilename = list(str(outputPath).split("/"))
         print("dummy")
 
     else:
         prs.save(outputPath)
 
+
 def main():
-    if args.input != None:
+    if args.input is not None:
         signCount = 0
         callCount = 0
         prs = Presentation()
@@ -41,7 +61,7 @@ def main():
         title_slide = prs.slides.add_slide(title_slide_layout)
         content_slide = []
         nn = 2
-        if args.pages == None:
+        if args.pages is None:
             print("pages flag should be number.")
             sys.exit(1)
         for i in range(int(args.pages) - 1):
@@ -79,10 +99,16 @@ def main():
                     content_title = content_slide[PCDraft].shapes.title
                     content_title.text = str(fileContent.replace("%%%", ""))
                 except(IndexError):
-                    print("Pages is not equal to section in draft file. exiting...")
+                    print(
+                          "Pages is not equal to section in draft file.",
+                          "exiting..."
+                         )
                     sys.exit(1)
                 except(UnboundLocalError):
-                    print("Slide did not have content pages but the draft file specify one. Existing.")
+                    print(
+                          "Slide did not have content pages but the draft ",
+                          "file specify one. Existing."
+                         )
                     sys.exit(1)
                 prs.save(outfilename)
 
@@ -98,43 +124,64 @@ def main():
             elif "bgcl>" in fileContent:
                 background = content_slide[PCDraft].background
                 try:
-                    R,G,B = str(fileContent.replace("bgcl>", "")).split(",")
+                    R, G, B = str(fileContent.replace("bgcl>", "")).split(",")
                 except(ValueError):
-                    print("background value should be RGB value seperate by comma. Example: bgcl>255,255,255")
+                    print(
+                          "background value should be RGB value seperate ",
+                          "by comma. Example: bgcl>255,255,255"
+                         )
                     sys.exit(1)
                 fill = background.fill
                 fill.solid()
                 try:
                     fill.fore_color.rgb = RGBColor(int(R), int(G), int(B))
                 except(ValueError):
-                    print("background value should be RGB value seperate by comma. Example: bgcl>255,255,255")
+                    print(
+                          "background value should be RGB value seperate by ",
+                          "comma. Example: bgcl>255,255,255"
+                         )
                     sys.exit(1)
                 prs.save(outfilename)
 
-
-#This function is not finish yet.
-#            elif "img>" in fileContent:
-#                con_shapes = content_slide[PCDraft].shapes
-#                try:
-#                    imgpath,x1,y1,x2,y2 = str(fileContent.replace("img>", "")).split(",")
-#                except(ValueError):
-#                    print("image value should be image path, Position of image horizontal, Position of image verticle, size of image horizontal, size of image verticle.")
-#                    sys.exit(1)
-#                try:
-#                    con_shapes.add_picture(str(imgpath), Inches(int(x1)), Inches(int(y1)), Inches(int(x2)), Inches(int(y2)))
-#                except(ValueError):
-#                    print("image value should be image path, Position of image horizontal, Position of image verticle, size of image horizontal, size of image verticle.")
-#                    sys.exit(1)
-#                except(FileNotFoundError):
-#                    print("image file did not exist. Please try again.")
-#                    sys.exit(1)
-#                prs.save(outfilename)
+            elif "img>" in fileContent:
+                con_shapes = content_slide[PCDraft].shapes
+                try:
+                    imgpath, x1, y1, x2, y2 = str(
+                                                  fileContent.replace("img>",
+                                                                      "")
+                                                 ).split(",")
+                except(ValueError):
+                    print(
+                          "image value should be image path, Position of ",
+                          "image horizontal, Position of image verticle, ",
+                          "size of image horizontal, size of image verticle."
+                         )
+                    sys.exit(1)
+                try:
+                    con_shapes.add_picture(
+                                           str(imgpath), Inches(int(x1)),
+                                           Inches(int(y1)), Inches(int(x2)),
+                                           Inches(int(y2))
+                                          )
+                except(ValueError):
+                    print(
+                          "image value should be image path, Position of ",
+                          "image horizontal, Position of image verticle, ",
+                          "size of image horizontal, size of image verticle."
+                         )
+                    sys.exit(1)
+                except(FileNotFoundError):
+                    print("image file did not exist. Please try again.")
+                    sys.exit(1)
+                prs.save(outfilename)
 
             elif "\n" or "\r\n" in fileContent:
                 pass
             else:
-                print("draft file is corrupted. Check the format.txt file and try again!")
-
+                print(
+                      "draft file is corrupted. Check the format.txt file ",
+                      "and try again!"
+                     )
 
             draft = f.readline()
     else:
